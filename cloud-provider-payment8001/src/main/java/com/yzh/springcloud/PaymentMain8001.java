d35a65c99a551cf7c9ebc7fe2455ce8cca5ae10b
yzh <yzhsmail@163.com>
2021-01-05 21:28:46 +0800

    17 - 21 : build eureka cluster and register service

--- cloud-provider-payment8001/src/main/java/com/yzh/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/yzh/springcloud/PaymentMain8001.java
@@ -2,6 +2,7 @@ package com.yzh.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author yzh
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Version 1.0
  * @description
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class PaymentMain8001 {
     public static void main(String[] args) {
