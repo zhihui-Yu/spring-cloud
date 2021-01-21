bf57799bddd782642138fc5a26ca2ca966b043af
yzh <yzhsmail@163.com>
2021-01-21 23:32:01 +0800

    24 - 27 : learn discovery client and eureka preservation

--- cloud-provider-payment8001/src/main/java/com/yzh/springcloud/controller/SimpleController.java
+++ cloud-provider-payment8001/src/main/java/com/yzh/springcloud/controller/SimpleController.java
@@ -7,6 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -23,6 +26,8 @@ public class SimpleController {
     private PaymentService paymentService;
     @Value("${server.port}")
     private String port;
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping("/payment")
     public CommonResult create(@RequestBody Payment payment) {
@@ -46,5 +51,23 @@ public class SimpleController {
         } else {
             return new CommonResult(500, "failed", null);
         }
+    }
+
+    @GetMapping("/payment/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        logger.info("========== services ==============");
+        for (String service : services) {
+            logger.info("service: " + service);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        logger.info("========== instances ==============");
+        for (ServiceInstance serviceInstance : instances) {
+            logger.info("serviceId" + serviceInstance.getServiceId());
+            logger.info("host: " + serviceInstance.getHost());
+            logger.info("port: " + serviceInstance.getPort());
+            logger.info("uri: " + serviceInstance.getUri());
+        }
+        return discoveryClient;
     }
 }
