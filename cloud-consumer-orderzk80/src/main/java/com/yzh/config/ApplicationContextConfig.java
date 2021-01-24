8917439156ea416a8a5e85df3b531fa0810fd580
yzh <yzhsmail@163.com>
2021-01-24 22:31:42 +0800

    bug-fixed: find should not delete module "cloud-consumer-orderzk80"

---
+++ cloud-consumer-orderzk80/src/main/java/com/yzh/config/ApplicationContextConfig.java
@@ -0,0 +1,18 @@
+package com.yzh.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author simple
+ */
+@Configuration
+public class ApplicationContextConfig {
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
